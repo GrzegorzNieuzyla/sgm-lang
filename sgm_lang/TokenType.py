20493cf8f2fc0f4e73a0c7aafa087a44a875b9f3
Marcin Kozak <marcinkozak005@gmail.com>
2020-03-27 19:29:01 +0100

    Tokenizer tokenizeSpecialCharacter finished

--- sgm_lang/TokenType.py
+++ sgm_lang/TokenType.py
@@ -1,9 +1,33 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class TokenType(Enum):
+
     INT = 1
     FLOAT = 2
+
+    L_BRACE = auto()
+    R_BRACE = auto()
+    L_PAREN = auto()
+    R_PAREN = auto()
+
+    COMMENT = auto()
+    ASSIGN = auto()
+    EQUAL = auto()
+    LESS = auto()
+    GREATER = auto()
+    LESS_EQUAL = auto()
+    GREATER_EQUAL = auto()
+
+    OR = auto()
+    AND = auto()
+    NOT = auto()
+
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    DIV = auto()
+    MOD = auto()
 
     def __repr__(self):
         return str(self.name)
--- sgm_lang/tokenizer.py
+++ sgm_lang/tokenizer.py
@@ -1,6 +1,8 @@
 from sgm_lang.TokenType import TokenType
 
-specialCharacters = ['{', '}', '(', ')']  # ...
+
+class TokenizerError(Exception):
+    pass
 
 
 def tokenizeNumber(code, position):
@@ -17,11 +19,75 @@ def tokenizeNumber(code, position):
         return position, (TokenType.FLOAT, number)
 
 
+def tokenizeSpecialCharacter(code, position):
+    currentElement = code[position]
+    if currentElement == '{':
+        token = TokenType.L_BRACE
+    elif currentElement == '}':
+        token = TokenType.R_BRACE
+    elif currentElement == '(':
+        token = TokenType.L_PAREN
+    elif currentElement == ')':
+        token = TokenType.R_PAREN
+    elif currentElement == '#':
+        token = TokenType.COMMENT
+    elif currentElement == '=':
+        position += 1
+        if position < len(code) and code[position] == '=':
+            token = TokenType.EQUAL
+        else:
+            position -= 1
+            token = TokenType.ASSIGN
+    elif currentElement == '<':
+        position += 1
+        if position < len(code) and code[position] == '=':
+            token = TokenType.LESS_EQUAL
+        else:
+            position -= 1
+            token = TokenType.LESS
+    elif currentElement == '>':
+        position += 1
+        if position < len(code) and code[position] == '=':
+            token = TokenType.GREATER_EQUAL
+        else:
+            position -= 1
+            token = TokenType.GREATER
+    elif currentElement == '|':
+        position += 1
+        if position < len(code) and code[position] == '|':
+            token = TokenType.OR
+        else:
+            raise TokenizerError("Single \'|\' found")
+    elif currentElement == '&':
+        position += 1
+        if position < len(code) and code[position] == '&':
+            token = TokenType.AND
+        else:
+            raise TokenizerError("Single \'&\' found")
+    elif currentElement == '!':
+        token = TokenType.NOT
+    elif currentElement == '+':
+        token = TokenType.ADD
+    elif currentElement == '-':
+        token = TokenType.SUB
+    elif currentElement == '*':
+        token = TokenType.MUL
+    elif currentElement == '/':
+        token = TokenType.DIV
+    elif currentElement == '%':
+        token = TokenType.MOD
+    else:
+        raise TokenizerError("Unknown character")
+    position += 1
+    return position, (token, None)
+
+
 def tokenize(code):
     tokensList = []
     position = 0
     while position < len(code):
         currentElement = code[position]
+
         if currentElement.isspace():
             pass
         elif currentElement.isalpha():
@@ -29,11 +95,13 @@ def tokenize(code):
         elif currentElement.isdigit():
             position, token = tokenizeNumber(code, position)
             tokensList.append(token)
-        elif currentElement in specialCharacters:
-            print(f'{currentElement} <- is special character')
+        else:
+            position, token = tokenizeSpecialCharacter(code, position)
+            tokensList.append(token)
         position += 1
 
     return tokensList
 
 
 print(tokenize("123.231 12 0.1 1."))
+print(tokenize(" - + = == = { }"))
